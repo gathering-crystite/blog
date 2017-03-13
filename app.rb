a1749854d4adef5374a1fb12bd452113158dd588
Scott Eissfeldt <seissfeldt@sweetspotdiabetes.com>
2017-03-13 14:54:44 -0700

    Add index.erb file

--- app.rb
+++ app.rb
@@ -3,13 +3,8 @@ require 'sinatra/base'
 
 class MyApp < Sinatra::Base
 
-  get '/' do
-    'Hello World!'
-  end
-
-get '/download/*.*' do
-  # matches /download/path/to/file.xml
-  params['download/alpine-minirootfs-3.5.1-x86_64.tar.gz'] # => ["path/to/file", "xml"]
+get '/' do
+  erb :index
 end
 
 end
