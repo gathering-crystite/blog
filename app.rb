03c3eaae980c4b763413965cad693b5060b82c1c
Scott Eissfeldt <seissfeldt@sweetspotdiabetes.com>
2017-04-10 08:42:23 -0700

    create public folder, set public folder in app

--- app.rb
+++ app.rb
@@ -3,8 +3,25 @@ require 'sinatra/base'
 
 class MyApp < Sinatra::Base
 
+  configure do
+    set :root, File.dirname(__FILE__)
+    set :public_folder, 'public'
+  end
+
+
 get '/' do
   erb :index
 end
+
+get '/about' do
+  erb :about
+end
+
+set :foo, 'bar'
+
+get '/foo' do
+  "foo is set to " + settings.foo
+end
+
 
 end
